6d5b3d45a201ab4186d95afbf5829474dfde8f65
Dimitris <dimopoulosvtec@yahoo.gr>
2020-04-12 00:53:27 +0300

    Accessing, Indexing and Using elements in a list

---
+++ chapter03/bicycle.py
@@ -0,0 +1,9 @@
+bicycles = ['trek', 'connonadale', 'redline', 'specialized']
+print(bicycles)
+
+print(bicycles[1])
+print(bicycles[3])
+print(bicycles[-1])
+
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
