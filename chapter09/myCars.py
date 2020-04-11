928594b710bf5913875e56c61558fe1a07c020fb
Dimitris <dimopoulosvtec@yahoo.gr>
2020-04-12 00:24:42 +0300

    Playing with imports

---
+++ chapter09/myCars.py
@@ -0,0 +1,7 @@
+from car import Car, ElectricCar
+
+civic = Car("honda", "civic", 2012)
+print(civic.getDescripteveName())
+
+myTesla = ElectricCar("tesla", "roadster", 2016)
+print(myTesla.getDescripteveName())
