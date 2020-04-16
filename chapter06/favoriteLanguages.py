89f56470f0b22ad418cd0f88b64cd2b586165ce6
Dimitris <dimopoulosvtec@yahoo.gr>
2020-04-17 02:57:14 +0300

    A dictionary of similar Objects and how to break up a long print statement

---
+++ chapter06/favoriteLanguages.py
@@ -0,0 +1,12 @@
+# A dictionary of similar Objects
+
+favoriteLanguages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+print("Sarah's favorite language is " +
+    favoriteLanguages['sarah'].title() +
+    ".")
