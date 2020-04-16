e1ed0d757127caf76fe035ee4762089f358db9cf
Dimitris <dimopoulosvtec@yahoo.gr>
2020-04-16 15:53:02 +0300

    Checking whether a value is NOT IN a list

---
+++ chapter05/bannedUsers.py
@@ -0,0 +1,7 @@
+# Checking whether a value is not in a list
+
+bannedUsers = ['andrew', 'carolina', 'david']
+user = 'marie'
+
+if user not in bannedUsers:
+    print(user.title() + ", you can post a response if you wish")
