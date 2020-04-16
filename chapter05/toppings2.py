da6f5d14b3903ce9772bd596d0519335b7ed7305
Dimitris <dimopoulosvtec@yahoo.gr>
2020-04-16 16:18:50 +0300

    Testing multiple conditions

---
+++ chapter05/toppings2.py
@@ -0,0 +1,12 @@
+# Testing multiple conditions
+
+requestedToppings = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in requestedToppings:
+    print("Adding mushrooms")
+if 'pepperoni' in requestedToppings:
+    print("Adding pepperoni")
+if 'extra cheese' in requestedToppings:
+    print("Adding extra cheese")
+
+print("\nFinished making pizza!")
