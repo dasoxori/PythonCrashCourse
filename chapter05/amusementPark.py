c049f3b04230ff70cb0cb181eac2c118ecb3d02f
Dimitris <dimopoulosvtec@yahoo.gr>
2020-04-16 16:12:29 +0300

    if - elif - else statement

---
+++ chapter05/amusementPark.py
@@ -0,0 +1,14 @@
+# if - elif - else statement
+
+age = 66
+
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 5
+elif age < 65:
+    price = 10
+else:
+    price = 5
+
+print("Your admission cost is " + str(price) + " $.")
