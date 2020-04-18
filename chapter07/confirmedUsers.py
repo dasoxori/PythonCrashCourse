c525535fbfde0b2ddc29e54b89967a20ded92a3f
Dimitris <dimopoulosvtec@yahoo.gr>
2020-04-18 16:30:58 +0300

    Using break to Exit a loop

---
+++ chapter07/confirmedUsers.py
@@ -0,0 +1,19 @@
+# Start with users that need to be verified,
+# and an empty list to hold confirmed users.
+
+unconfirmedUsers = ['alise', 'brian', 'candace']
+confirmedUsers = []
+
+# Verify each user until there are no more unconfirmed users.
+# Move each verified user into the list of confirmed users.
+
+while unconfirmedUsers:
+    currentUser = unconfirmedUsers.pop()
+
+    print("Verifying user: " + currentUser.title())
+    confirmedUsers.append(currentUser)
+
+# Display all confirmed users.
+print("\nThe following users have been confirmed: ")
+for confirmedUser in confirmedUsers:
+    print(confirmedUser.title())
