53527db5065053ef99590bed62dc61356594f873
Dimitris <dimopoulosvtec@yahoo.gr>
2020-04-18 16:59:07 +0300

    Define a function and passing information

---
+++ chapter08/greeter.py
@@ -0,0 +1,7 @@
+# defining a function
+
+def greetUser(username):
+    """Display a simple greeting.""" # docstring, descripes what the function does.
+    print("Hello, " + username.title() + "!")
+
+greetUser('jesse')
