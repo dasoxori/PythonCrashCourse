872cf49c67c03f8a2459b141e3ffeb5c44f171b1
Dimitris <dimopoulosvtec@yahoo.gr>
2020-04-20 15:47:05 +0300

    Using a Function with a while loop

--- chapter08/greeter.py
+++ chapter08/greeter.py
@@ -5,3 +5,27 @@ def greetUser(username):
     print("Hello, " + username.title() + "!")
 
 greetUser('jesse')
+
+
+
+# Using a Function with a while loop
+print("Using a Function with a while loop and break statement")
+
+def getFormattedName(firstName, lastName):
+    """Return a full name, neatly formatted."""
+    fullName = firstName + " " + lastName
+    return fullName.title()
+
+# While loop with break statement
+while True:
+    print("\nPlease tell me your name:\n(enter 'q' at any time to quit)\n")
+    firstName = input("First name: ")
+    if firstName == 'q':
+        break
+    
+    lastName = input("Last name: ")
+    if lastName == 'q':
+        break
+
+    formattedName = getFormattedName(firstName, lastName)
+    print("\nHello, " + formattedName)
