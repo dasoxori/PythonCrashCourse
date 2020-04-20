c958c20372ed744f4eb238fe93de89237e2f3c4a
Dimitris <dimopoulosvtec@yahoo.gr>
2020-04-20 17:04:32 +0300

    Remove Chapter09

--- chapter09/myCars.py
+++
@@ -1,7 +0,0 @@
-from car import Car, ElectricCar
-
-civic = Car("honda", "civic", 2012)
-print(civic.getDescripteveName())
-
-myTesla = ElectricCar("tesla", "roadster", 2016)
-print(myTesla.getDescripteveName())
